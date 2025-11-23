f03480da1e68b7459bd16f154379d2f1aed64c3e
Farhia001 <h24farhi@du.se>
2025-11-23 17:50:56 +0100

    Refactor script.js to remove old comments and code

--- script.js
+++ script.js
@@ -43,13 +43,14 @@ console.log(undefined ? 'truthy': 'falsy');
 /* Reflektion uppgift 3 */
 /* 1. == gör typkonvertering, vilket betyder att JavaScript försöker omvandla värden
       till samma datatyp innan jämförelsen. Därför blir t.ex. 2 == "2" sant.
-      === är strikt likhet. JavaScript jämför både värde och datatyp, utan att omvandla något.*/
+      === är strikt likhet, JavaScript jämför både värde och datatyp, utan att omvandla något.*/
 
 /* 2. I både ternary operator och if-satser utvärderas uttrycket som en boolean.
       Det spelar ingen roll vad värdet är från början — JavaScript avgör om det är truthy eller falsy. */
       
-/* 3. NaN betyder inte ett nummer och NaN kommer aldrig vara lika med NaN trots om det är == eller ===.
-      Undefined */
+/* 3. NaN står för "Not a Number" och används när en matematisk operation ger ett
+      ogiltigt tal och är inte lika med något även sig själv. Undefined betyder att något saknar värde. T.ex. när en variabel är
+      deklarerad men inte tilldelad. null är tomt värde. Används för att visa att en variabel ska vara tom eller inte innehåller något just nu.*/
 
 
 /* Uppgift 4 - Funktioner */
@@ -64,16 +65,17 @@ console.log(greetValue);
 
 
 /* Reflektion uppgift 4 */
-/* 1. - Funktionsdeklaration så använder man ordet function och sedan namnet på funktion 
+/* 1. - Funktionsdeklaration så använder man ordet function och sedan namnet på funktion, man anropa innan funktion deklareras.
       - För funktionsuttryck har man en varibel med funktion som värde. Man kan inte anropa innan funktionen skapas
       - Arrowfunction är kortare formatering av funktion. Man kan inte heller anropa innan funktionen skapas.
        */
 /* 2. För funktionsdeklartion så spelar det ingen roll om man anropar funktionen innan eller efter funktion
       skapats. */
 
-/* 3. När jag anropar funktionen i name let varibeln så händer det inget med funktionen.
-      Om jag ändrar i parameter name med värdet 'Farhia' till något annat anropas det istället. */
+/* 3. När jag anropar funktionen i 'name' let varibeln så händer det inget med funktionen.
+      Om jag ändrar i parameter 'name' med värdet 'Farhia' till något annat anropas det istället. */
 
 /* 4. Parameter är ett namn som används i funktionen inom parenteser som tar emot värde. 
       Argumenter är värdet som tas emot av funktionen när man anropar den.
+
       Och varibel är en namngivaren behållare för ett värde. */
